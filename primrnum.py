633cfa62b57366fa935907dc672918fc73abb392
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-26 08:47:29 +0530

    Add files via upload

--- primrnum.py
+++ primrnum.py
@@ -1,7 +1,5 @@
 n = int(input())
-for i in range(2,n):
-    if (n % i == 0):
-        print("not")
-        break
-    print("prime")
-    break
+
+for i in range(2, n):
+    if n % i == 0:
+        print(i)
