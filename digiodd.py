84694f633b80897caff29d5a9b99953363754703
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-26 08:47:15 +0530

    Add files via upload

---
+++ digiodd.py
@@ -0,0 +1,11 @@
+n = list(map(str,input().split()))     #dw f g feb g gb qte b e brq ab  fdbva
+print(n)    #['dw', 'f', 'g', 'feb', 'g', 'gb', 'qte', 'b', 'e', 'brq', 'ab', 'fdbva']
+p = len(n)
+strt = []
+for i in range(p):
+    strt +=n[i]         
+print(strt)       #['d', 'w', 'f', 'g', 'f', 'e', 'b', 'g', 'g', 'b', 'q', 't', 'e', 'b', 'e', 'b', 'r', 'q', 'a', 'b', 'f', 'd', 'b', 'v', 'a']
+
+strt[-1]
+    
+
